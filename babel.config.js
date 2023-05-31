efaaabc0ef41ad3da66857265610852d3c72070e
José Miguel Gonçalves <jose.goncalves@inov.pt>
2023-05-31 10:20:21 +0100

    Migrated to vite.

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-
-module.exports = {
-	presets: [
-		'@vue/cli-plugin-babel/preset'
-	]
-};
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
-	transpileDependencies: true,
-	publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
-	productionSourceMap: false
-});
