f4a0d171eba66e258be917f3fcd8b6e35b2814ad
José Miguel Gonçalves <jose.goncalves@inov.pt>
2024-05-29 17:12:42 +0100

    Changed server port to 8080 and set to automatically open the app in the browser.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		open: true,
+		port: 8080
+	},
 	plugins: [vue()],
 	resolve: {
 		alias: {
