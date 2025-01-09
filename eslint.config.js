eeb20cc0417fbc1fcbc1a1a0e63c4198db6c200f
José Miguel Gonçalves <jose.goncalves@inov.pt>
2025-01-09 17:51:35 +0000

    Upgraded Vite to v6 and ESLint to v9

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+
+export default [
+	{
+		name: 'app/files-to-lint',
+		files: ['**/*.{js,mjs,jsx,vue}'],
+	},
+
+	{
+		name: 'app/files-to-ignore',
+		ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+	},
+
+	js.configs.recommended,
+	...pluginVue.configs['flat/recommended'],
+
+	{
+		rules: {
+			'indent': ['warn', 'tab'],
+			'vue/html-indent': ['warn', 'tab'],
+			'quotes': ['warn', 'single'],
+			'semi': ['warn', 'always']
+		}
+	}
+];
