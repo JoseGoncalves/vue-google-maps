5ee33e27369770b67104b2b99d069d6d3c6112d0
José Miguel Gonçalves <jose.goncalves@inov.pt>
2025-07-08 00:41:07 +0100

    Updated vue3-google-map to v0.23.0. Updated ESLint config. Updated other dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,22 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
 import js from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
 
-export default [
+export default defineConfig([
 	{
 		name: 'app/files-to-lint',
 		files: ['**/*.{js,mjs,jsx,vue}'],
 	},
 
+	globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
 	{
-		name: 'app/files-to-ignore',
-		ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
 	},
 
 	js.configs.recommended,
@@ -23,4 +30,4 @@ export default [
 			'semi': ['warn', 'always']
 		}
 	}
-];
+]);
